831bad57c05304551a8573048c4f82e7a18b401a
Bernd Plontsch <bernd.plontsch@gmail.com>
2025-08-12 22:07:24 +0900

    Initial Commit

---
+++ Stack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stack
+//
+//  Created by Bernd Plontsch on 12.08.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stack/StackApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackApp.swift
+//  Stack
+//
+//  Created by Bernd Plontsch on 12.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct StackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
